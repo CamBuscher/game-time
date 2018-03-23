5a5e0b64c7f4bd7c334c786ec03be8b1278fb937
Cameron Buscher <cameronbuscher@gmail.com>
2018-03-22 22:32:09 -0600

    Correct bricks tests
    And fixed level three functions as a result of tests. Hooray tests.

--- test/bricks-test.js
+++ test/bricks-test.js
@@ -9,13 +9,13 @@ describe('Brick', function() {
 
   it('create bricks function should create a lot of bricks', function() {
     let brick = new Brick(5, 5);
-    let bricksArray = brick.createBricks(20, 1);
+    let bricksArray = brick.createBricksLvlsOneTwo(20, 1);
     assert.equal(bricksArray.length, 20);
   })
 
   it('should create 16 normal bricks on level 3', function() {
     let brick = new Brick();
-    let bricksArray = brick.createBricks(60, 3);
+    let bricksArray = brick.createBricksLvlThree(60);
     assert.equal(bricksArray.length, 48);
     let normalBricks = bricksArray.filter( brick => brick.health === 1)
     assert.equal(normalBricks.length, 16);
@@ -23,7 +23,7 @@ describe('Brick', function() {
 
   it('should create 32 strong bricks on level 3', function() {
     let brick = new Brick();
-    let bricksArray = brick.createBricks(60, 3);
+    let bricksArray = brick.createBricksLvlThree(60);
     assert.equal(bricksArray.length, 48);
     let strongerBricks = bricksArray.filter( brick => brick.health === 2)
     assert.equal(strongerBricks.length, 32);
@@ -31,7 +31,7 @@ describe('Brick', function() {
 
   it('clear bricks function should be able to clear it\'s bricks array', function() {
     let brick = new Brick();
-    let bricksArray = brick.createBricks(60, 3);
+    let bricksArray = brick.createBricksLvlThree(60);
     assert.equal(bricksArray.length, 48);
     bricksArray = brick.clearBricks();
     assert.equal(bricksArray.length, 0);
